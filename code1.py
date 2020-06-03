f29e0e35fc3b9fd1086a496383e3ee7d8493950b
Ahmed <65268218+AhmedAlkhafai99@users.noreply.github.com>
2020-06-03 12:23:43 -0700

    Are all bugs fixed?

--- code1.py
+++ code1.py
@@ -9,3 +9,20 @@ def lone_sum(a, b, c):
         return 0
     else:
         return a+b+c
+
+    -----------
+    --
+    -------
+def lone_sum(a, b, c):
+    if a == b and a == c and b == c:
+        return c
+    elif a >= b:
+        return b
+    elif b == c:
+        return a
+    elif a == c:
+        return b
+    else:
+        print(lone_sum(3, 5, 7))
+  
+#Have a good day
